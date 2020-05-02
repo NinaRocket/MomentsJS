224894987fd006e7f7b1aaab7ae7b24fcd6b2b9f
NRocket <ninamrocket@gmail.com>
2020-05-02 09:03:02 -0700

    css class wip

--- Develop/script.js
+++ Develop/script.js
@@ -20,17 +20,46 @@ var current = m.toString();
 $("#currentDay").text(current);
 console.log(current);
 
+//var h = m.hour(9);
 
 //setting units m.minutes(52);
 
-
+var currentHour = m.hour();
+console.log(currentHour);
 
 //color code each block, dynamic
 
-// var r = $("row");
-// r.addClass("row");
-// var planRow = $("row").append(r);
-// $("container").append(planRow);
+var element = document.querySelector("time-block");
+
+// style = window.getComputedStyle(element),
+// top = style.getPropertyValue('top');
+
+//set hour for each column to compare time to
+
+var h9 = $("#nineAM");
+h9 = 9;
+console.log(h9);
+var planHour = $(".hour");
+
+if (currentHour < h9) {
+
+
+    $("#nineAM").addClass("future");
+
+} else if (currentHour === h9) {
+    $("#nineAM").addClass("present");
+}
+else
+    $("#nineAM").addClass("past");
+
+console.log("no");
+
+var h10 = $("#tenAM");
+h10 = 10;
+if (currentHour < h10) {
+    $("#tenAM").addClass("future");
+}
+
 
 
 //eventlistener (onclick) for each timeblock
