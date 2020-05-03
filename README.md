ede6684d784126307d21d3c4509efac5d59835d4
NRocket <ninamrocket@gmail.com>
2020-05-03 12:18:15 -0700

    save button icon and readme

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # MomentsJS
+
+This web application allows a user to store meetings and events for their day. The events are stored in local storage so that the user may refresh the page and the events remain in their day planner. Moment js is used to display the current date. 
+
+Events that have past are gray, events that are occurring in the present are red, events that are in the future are green.
+
--- script.js
+++ script.js
@@ -21,25 +21,12 @@ function persist() {
 
 //curent date and time
 let m = moment();
+var dayDate = m.format('MMMM Do YYYY');
+var dayNum = m.day();
+var dayString = m.day(dayNum).format("dddd");
 
-//console.log("toString() => ${ m.toString() }");
-//m.set("day", 4);
+$("#currentDay").text(dayString + " " + dayDate);
 
-// using a format
-//m = moment("05/01/2019 6:55PM", "MM/DD/YYYY h:mmA");
-// console.log(m.toString());
-// //getting units
-// console.log(m.minutes());
-// console.log(m.hour());
-// console.log(m.week());
-// console.log(m.get("quarter"));
-var current = m.toString();
-$("#currentDay").text(current);
-//console.log(current);
-
-//var h = m.hour(9);
-
-//setting units m.minutes(52);
 
 var currentHour = m.hour();
 //loops through rows to compare times for block color
